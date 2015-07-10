1081bc8680bfc93683ee4f3d1d74a212dd6d1449
Stetsenko Makar <makazone@gmail.com>
2015-07-09 20:18:01 -0700

    Implement collection view for search screen

--- Symphonic/Symphonic/Controller/SearchShowsVC.swift
+++ Symphonic/Symphonic/Controller/SearchShowsVC.swift
@@ -10,18 +10,51 @@ import UIKit
 
 class SearchShowsVC: UICollectionViewController {
     private let kShowCellIdentifier = "showGridIdentifier"
+    private let insets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+
+    
+    var shows = [Show]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        Show.getShowList()
+
+        collectionView?.registerNib(UINib(nibName: "showCell", bundle: nil), forCellWithReuseIdentifier: kShowCellIdentifier)
+
+        shows = Show.getShowList()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
+extension SearchShowsVC: UICollectionViewDataSource {
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return shows.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(kShowCellIdentifier, forIndexPath: indexPath) as! showCellView
+
+        cell.showImage.image = UIImage(named: shows[indexPath.row].imageName)
+        cell.showTitle.text  = shows[indexPath.row].name
+
+        return cell
+    }
+}
+
+extension SearchShowsVC : UICollectionViewDelegateFlowLayout {
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let cellWidth  = (collectionView.frame.width - 3*insets.left)/2
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return insets
+    }
+}
