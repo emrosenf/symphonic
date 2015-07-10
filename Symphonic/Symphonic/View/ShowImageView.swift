9242a26082a4cf06fa8c0ddb1e5f8f8319ed0bd7
Evan Rosenfeld <evan@DN0a1819b2.SUNet>
2015-07-09 17:51:42 -0700

    views

---
+++ Symphonic/Symphonic/View/ShowImageView.swift
@@ -0,0 +1,29 @@
+//
+//  ShowImageView.swift
+//  Symphonic
+//
+//  Created by Evan Rosenfeld on 7/9/15.
+//  Copyright (c) 2015 Avocado Hills. All rights reserved.
+//
+
+import UIKit
+
+class ShowImageView: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var venueLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    /*
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
